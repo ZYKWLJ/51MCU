ebcb23adf275bf0949887f905fdc3d8d6ea75901
郑阳康 <2126483838@qq.com>
2025-04-23 21:18:37 +0800

    定时器自动生成的

--- 8-定时器/main.c
+++ 8-定时器/main.c
@@ -1,34 +1,34 @@
 #include <REGX52.h>
-void timer_init_0()
+void Timer0_Isr(void) interrupt 1
 {
-    TMOD = 0x01;          // 工作在模式1，0000 0001
-    TF0 = 0;              // 中断标志位初始值为0
-    TR0 = 1;              // 定时器0开始运行
-    TH0 = 64536 >> 8;     // 中断计时高8位赋初值，相当于64535/256
-    TL0 = 64536 & 0x00FF; // 中断计时低8位赋初值，相当于64535%256
-    ET0 = 1;              // 允许定时器0中断
-    EA = 1;               // 允许总中断
-    PT0 = 0;              // 低优先级
+    unsigned int T0Count;
+    TL0 = 0x18; // 设置定时初始值
+    TH0 = 0xFC; // 设置定时初始值
+    T0Count++;
+    if (T0Count == 100)
+    {
+        T0Count = 0;
+        P2_0 = ~P2_0;
+    }
+}
+
+void Timer0_Init(void) // 1毫秒@12.000MHz
+{
+    // AUXR &= 0x7F;			//定时器时钟12T模式
+    TMOD &= 0xF0; // 设置定时器模式
+    TMOD |= 0x01; // 设置定时器模式
+    TL0 = 0x18;   // 设置定时初始值
+    TH0 = 0xFC;   // 设置定时初始值
+    TF0 = 0;      // 清除TF0标志
+    TR0 = 1;      // 定时器0开始计时
+    ET0 = 1;      // 使能定时器0中断
+    EA = 1;       // 使能总中断
 }
 
 void main()
 {
-    timer_init_0();
+    Timer0_Init();
     while (1)
     {
-    }
-}
-
-void Timer0_Routine(void) interrupt 1//这是定时器0的中断函数,内置的，我们按着实现就好！
-{
-    unsigned int T0Count;
-    // 达到了1ms，重新计时下一个1ms
-    TH0 = 64536 >> 8;     // 中断计时高8位赋初值，相当于64535/256
-    TL0 = 64536 & 0x00FF; // 中断计时低8位赋初值，相当于64535%256
-    T0Count++;
-    if (T0Count == 1000)
-    {
-        T0Count = 0;
-        P2_0 = ~P2_0;
     }
 }
